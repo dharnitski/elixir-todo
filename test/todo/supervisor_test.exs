08bcf56ea7fdc3755049c6e880f5b16a5602872c
Dmitry Harnitski <dmitry@brainjocks.com>
2016-06-19 10:47:06 -0400

    check DB process recovery in Supervisor

--- test/todo/supervisor_test.exs
+++ test/todo/supervisor_test.exs
@@ -1,16 +1,16 @@
 defmodule Todo.Supervisor.Test do
   use ExUnit.Case
 
-  setup do
-    case GenServer.whereis(:todo_cache) do
-      nil -> :ok
-      pid ->
-        GenServer.stop(pid)
-    end
-  end
-
-  test "Supervisor" do
+  test "Db Worker Recovery" do
     Todo.Supervisor.start_link
+    worker = Todo.ProcessRegistry.whereis_name({:database_worker, 2})
+    assert Process.alive?(worker)
+    Process.exit(worker, :kill)
+    assert Process.alive?(worker) == false
+    # give some time to superviser to restart worker
+    :timer.sleep(100)
+    new_worker = Todo.ProcessRegistry.whereis_name({:database_worker, 2})
+    assert Process.alive?(new_worker)
   end
 
 end
