202186eadbda1431b42529f47660e7bbed028ba2
Dmitry Harnitski <dmitry@brainjocks.com>
2016-06-19 18:18:37 -0400

    add test to cover loading server from todo cache process

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -21,13 +21,22 @@ defmodule Todo.Cache.Test do
     :ok
   end
 
-  test "Cache" do
+  test "Cache to reuse processes" do
     {:ok, _} = Todo.Cache.start_link
     bobs1 = Todo.Cache.server_process("Bob's list")
+    #load server in client process
     bobs2 = Todo.Cache.server_process("Bob's list")
     alice1 = Todo.Cache.server_process("Alice's list")
     assert bobs1 == bobs2
     assert bobs1 != alice1
+  end
+
+  test "Cache to check Server in todo process" do
+    {:ok, _} = Todo.Cache.start_link
+    bobs1 = Todo.Cache.server_process("Bob's list")
+    #load server in to-do cache process
+    bobs2 = GenServer.call(:todo_cache, {:server_process, "Bob's list"})
+    assert bobs1 == bobs2
   end
 
   test "Use cache for ToDo list" do
--- test/todo/supervisor_test.exs
+++ test/todo/supervisor_test.exs
@@ -16,7 +16,7 @@ defmodule Todo.Supervisor.Test do
     assert Process.alive?(worker)
     Process.exit(worker, :kill)
     assert Process.alive?(worker) == false
-    # give some time to superviser to restart worker
+    # give some time to supervisor to restart worker
     :timer.sleep(100)
     new_worker = Todo.ProcessRegistry.whereis_name({:database_worker, 2})
     assert Process.alive?(new_worker)
