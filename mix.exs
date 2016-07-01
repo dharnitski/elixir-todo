7ab0861bbe800abe74dfd61e4c6e29ae0f19792d
Dmitry Harnitski <dmitry@brainjocks.com>
2016-06-30 22:01:38 -0400

    Add web module with not tested methods

--- mix.exs
+++ mix.exs
@@ -21,15 +21,6 @@ defmodule Todo.Mixfile do
     [applications: [:logger, :gproc, :cowboy, :plug]] ++ rest
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:gproc, "0.3.1"},
