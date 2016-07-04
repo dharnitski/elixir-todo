0fc79cee7aef65db16454eab2dd9632fc0f88301
Dmitry Harnitski <dmitry@brainjocks.com>
2016-07-03 23:56:31 -0400

    test Web add entry

--- test/todo/web_test.exs
+++ test/todo/web_test.exs
@@ -37,6 +37,26 @@ defmodule Todo.Web.Test do
     end
   end
 
+  test "add entry" do
+    with_mock Todo.Cache,
+      [server_process: fn(_) -> nil end] do
+      with_mock Todo.Server,
+        [add_entry: fn(_, %{date: {2013, 12, 19}, title: "Dentist"}) -> nil end] do
+
+        conn = conn(:post, "/add_entry?list=bob&date=20131219&title=Dentist", "")
+          |> Todo.Web.call(@opts)
+
+        assert conn.state == :sent
+        assert conn.status == 200
+      end
+    end
+  end
+
+  test "Application Start" do
+    Todo.Web.start_server
+    assert :ok = Plug.Adapters.Cowboy.shutdown(Todo.Web.HTTP)
+  end
+
   test "returns 404" do
     conn = conn(:get, "/missing", "")
            |> Todo.Web.call(@opts)
